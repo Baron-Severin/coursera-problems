249dd938f40a9ff5acf558112cc083335bab04cd
Baron-Severin <severinrudie@gmail.com>
2018-06-20 18:52:24 -0700

    Added fast ReverseInteger implementation

--- src/main/leetcode/7-ReverseInteger.kt
+++ src/main/leetcode/7-ReverseInteger.kt
@@ -1,5 +1,7 @@
 package leetcode
 
+import java.lang.Math.abs
+
 fun reverse(x: Int): Int {
   var str = x.toString()
   var neg = false
@@ -15,4 +17,25 @@ fun reverse(x: Int): Int {
   } catch (e: Exception) {
     0
   }
+}
+
+fun reverseFast(x: Int): Int {
+  val neg = x < 0
+  var x : Long = abs(x.toLong())
+  val list = mutableListOf<Long>()
+
+  while (x >= 10) {
+    list += x % 10
+    x /= 10
+  }
+  list += x
+  var reversed : Long = 0
+  while (list.isNotEmpty()) {
+    reversed *= 10
+    reversed += list.removeAt(0)
+  }
+  if (reversed > Integer.MAX_VALUE) return 0
+  if (neg) reversed = -reversed
+
+  return reversed.toInt()
 }
--- src/test/leetcode/_7_ReverseIntegerKtTest.kt
+++ src/test/leetcode/_7_ReverseIntegerKtTest.kt
@@ -1,7 +1,9 @@
 package leetcode
 
 import org.junit.jupiter.api.Test
+import kotlin.system.measureNanoTime
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 internal class _7_ReverseIntegerKtTest {
 
@@ -11,5 +13,30 @@ internal class _7_ReverseIntegerKtTest {
     assertEquals(-321, reverse(-123))
     assertEquals(21, reverse(120))
     assertEquals(0, reverse(1_534_236_469))
+    assertEquals(0, reverse(-2_147_483_648))
+  }
+
+  @Test
+  fun reverseFast() {
+    assertEquals(321, reverseFast(123))
+    assertEquals(-321, reverseFast(-123))
+    assertEquals(21, reverseFast(120))
+    assertEquals(0, reverseFast(1_534_236_469))
+    assertEquals(0, reverseFast(-2_147_483_648))
+  }
+
+  @Test fun speedTest() {
+    val slow = measureNanoTime {
+      reverse(Integer.MAX_VALUE / 10)
+    }
+    val fast = measureNanoTime {
+      reverseFast(Integer.MAX_VALUE / 10)
+    }
+    val pattern = "%1$15s"
+
+    println("Slow: ${pattern.format(slow)}")
+    println("Fast: ${pattern.format(fast)}")
+
+    assertTrue { fast < slow }
   }
 }
