bbac8276dd4be3a0c26269ef50a398fc8e18e93b
Baron-Severin <severinrudie@gmail.com>
2018-06-20 17:47:43 -0700

    Gave up and merged the two arrays in MedianOfTwoSortedArrays

--- src/main/leetcode/4-MedianOfTwoSortedArrays.kt
+++ src/main/leetcode/4-MedianOfTwoSortedArrays.kt
@@ -1,49 +1,45 @@
 package leetcode
 
-const val EVEN = 0
-const val ODD = 1
-
 class Solution {
 
   fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
-    // Find midpoint of each array
-    // Binary sort through in tandem
-    // If one index increases by 5, the other must decrease by 5
-    // If one pointer reaches the end of its collection, add the size of that collection to the other one.  find the middle and go x up/down
-
-    val state = if ((nums1.size + nums2.size) % 2 == 0) EVEN else ODD
-
-    var prev1 : Int? = null
-    var prev2 : Int? = null
-
-    var low1 = 0
-    var low2 = 0
-
-    var high1 = nums1.size - 1
-    var high2 = nums2.size - 1
-
-    while (true) {
-      val index1 = getIndex(high1, low1)
-      val mid1 = nums1[index1]
-      val index2 = getIndex(high2, low2)
-      val mid2 = nums2[index2]
-
-      if (mid1 > mid2) {
-        if (state == ODD && mid2 < mid1 && mid1 < nums2[index2 + 1]) return mid1.toDouble()
-        high1 = index1
-        low2 = index2
-      } else {
-        if (state == ODD && mid1 < mid2 && mid2 < nums1[index1 + 1]) return mid2.toDouble()
-        low1 = index1
-        high2 = index2
-      }
-    }
-
-
+    val all = merge(nums1, nums2)
+    val mid = all.size / 2
+    if (all.size % 2 == 1) return all[mid].toDouble()
+    return (all[mid].toDouble() + all[mid - 1].toDouble()) / 2
   }
 
-  private fun getIndex(high: Int, low: Int): Int {
-    return ((high - low) / 2) + low
+  fun merge(arr1: IntArray, arr2: IntArray): IntArray {
+    val result = IntArray(arr1.size + arr2.size)
+
+    var index1 = 0
+    var index2 = 0
+    var indexR = 0
+    while (index1 < arr1.size && index2 < arr2.size) {
+      when {
+        arr2[index2] <= arr1[index1] -> {
+          result[indexR] = arr2[index2]
+          index2++
+          indexR++
+        }
+        arr1[index1] <= arr2[index2] -> {
+          result[indexR] = arr1[index1]
+          index1++
+          indexR++
+        }
+      }
+    }
+    while (index1 < arr1.size) {
+      result[indexR] = arr1[index1]
+      index1++
+      indexR++
+    }
+    while (index2 < arr2.size) {
+      result[indexR] = arr2[index2]
+      index2++
+      indexR++
+    }
+    return result
   }
 
 }
--- src/test/leetcode/SolutionTest.kt
+++ src/test/leetcode/SolutionTest.kt
@@ -16,5 +16,24 @@ internal class SolutionTest {
     arr1 = intArrayOf(1, 3, 5, 7)
     arr2 = intArrayOf(2, 4, 8, 9, 10)
     assertEquals(5.0, s.findMedianSortedArrays(arr1, arr2))
+
+    arr1 = intArrayOf(1, 3, 5, 7, 8)
+    arr2 = intArrayOf(2, 4, 8, 9, 10)
+    assertEquals(6.0, s.findMedianSortedArrays(arr1, arr2))
+
+
+//    1 2 3 4
+//    5 7
+//    8 8 9 10
+  }
+
+  @Test fun testMerge() {
+    var arr1 = intArrayOf(1, 3)
+    var arr2 = intArrayOf(2)
+//    assertEquals(s.merge(arr1, arr2).toList(), listOf(1, 2, 3))
+
+    arr1 = intArrayOf(1, 3, 5, 6)
+    arr2 = intArrayOf(2, 3)
+    assertEquals(s.merge(arr1, arr2).toList(), listOf(1, 2, 3, 3, 5, 6))
   }
 }
