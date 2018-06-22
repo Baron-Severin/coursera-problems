d160a0bab56d9a0a02dba9a65e208c20d1f01a69
Baron-Severin <severinrudie@gmail.com>
2018-06-22 15:02:36 -0700

    Added TODO problems for offline (BART)

---
+++ src/main/leetcode/todo/23-MergeKLinkedLists.kt
@@ -0,0 +1,22 @@
+package leetcode.todo
+
+import leetcode.day2.ListNode
+
+
+/**
+ * HARD
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+Example:
+
+Input:
+[
+1->4->5,
+1->3->4,
+2->6
+]
+Output: 1->1->2->3->4->4->5->6
+ */
+fun mergeKLists(lists: Array<ListNode?>): ListNode? {
+return null
+}
