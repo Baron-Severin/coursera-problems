d71454a7a1dbeccf4690751152c4db33c43a4fe5
Baron-Severin <severinrudie@gmail.com>
2018-06-22 11:55:49 -0700

    Implemented faster MaximumProductOfWordLength (stolen from someone else's, wanted to understand how he did it)

--- src/main/leetcode/day2/318-MaximumProductOfWordLengths.kt
+++ src/main/leetcode/day2/318-MaximumProductOfWordLengths.kt
@@ -2,7 +2,30 @@ package leetcode.day2
 
 import kotlin.math.max
 
+
 fun maxProduct(words: Array<String>): Int {
+  val bits = mutableListOf<Int>()
+  var longest = 0
+  for (i in words.indices) {
+    bits.add(0)
+    for (char in words[i]) {
+      bits[i] = bits[i] or (1 shl (char - 'a'))
+    }
+  }
+
+  for (i in bits.indices) {
+    for (j in 1 until bits.size) {
+      if (i == j) continue
+      if (bits[i] and bits[j] == 0) {
+        longest = max(longest, words[i].length * words[j].length)
+      }
+    }
+  }
+  return longest
+}
+
+
+fun maxProductSlow(words: Array<String>): Int {
   val sortedWords = words.sortedBy { it.length }
   var biggest = 0
   sortedWords.indices
--- src/test/leetcode/day2/_318_MaximumProductOfWordLengthsKtTest.kt
+++ src/test/leetcode/day2/_318_MaximumProductOfWordLengthsKtTest.kt
@@ -1,19 +1,41 @@
 package leetcode.day2
 
 import org.junit.jupiter.api.Test
+import kotlin.system.measureNanoTime
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 internal class _318_MaximumProductOfWordLengthsKtTest {
 
   @Test fun one() {
     assertEquals(16, maxProduct(arrayOf("abcw","baz","foo","bar","xtfn","abcdef")))
+    assertEquals(16, maxProductSlow(arrayOf("abcw","baz","foo","bar","xtfn","abcdef")))
   }
 
   @Test fun two() {
     assertEquals(4, maxProduct(arrayOf("a","ab","abc","d","cd","bcd","abcd")))
+    assertEquals(4, maxProductSlow(arrayOf("a","ab","abc","d","cd","bcd","abcd")))
   }
 
   @Test fun three() {
     assertEquals(0, maxProduct(arrayOf("a","aa","aaa","aaaa")))
+    assertEquals(0, maxProductSlow(arrayOf("a","aa","aaa","aaaa")))
+  }
+
+  @Test fun time() {
+    val values = arrayOf("abcw","baz","foo","bar","xtfn","abcdef","abcw","baz","foo","bar","xtfn","abcdef","abcw",
+        "baz","foo","bar","xtfn","abcdef","abcw","baz","foo","bar","xtfn","abcdef","abcw","baz","foo","bar","xtfn",
+        "abcdef","abcw","baz","foo","bar","xtfn","abcdef","abcw","baz","foo","bar","xtfn","abcdef","abcw","baz",
+        "foo","bar","xtfn","abcdef","abcw","baz","foo","bar","xtfn","abcdef","abcw","baz","foo","bar","xtfn",
+        "abcdef","abcw","baz","foo","bar","xtfn","abcdef")
+    val pattern = "%10d"
+    val slow = pattern.format(measureNanoTime {
+      maxProductSlow(values)
+    })
+    val fast = pattern.format(measureNanoTime {
+      maxProduct(values)
+    })
+    println("Slow: $slow\nFast: $fast")
+    assertTrue { slow > fast }
   }
 }
