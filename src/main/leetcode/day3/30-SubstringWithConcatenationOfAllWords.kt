f3e35b70211b48840e8b8e00253f3fb3a9427ae3
Baron-Severin <severinrudie@gmail.com>
2018-06-22 20:00:47 -0700

    Implemented SubstringWithConcatenationOfAllWords

--- src/main/leetcode/day3/30-SubstringWithConcatenationOfAllWords.kt
+++ src/main/leetcode/day3/30-SubstringWithConcatenationOfAllWords.kt
@@ -1,10 +1,54 @@
 package leetcode.day3
 
+fun findSubstring(s: String, words: Array<String>): List<Int> {
+  if (s.isEmpty() || words.isEmpty()) return emptyList()
+  val wordKeys = words.map { it.hashCode() }.countOccurances()
+  val wordLen = words[0].length
+  val concatenatedLen = words.joinToString(separator = "").length
+
+  val indices = mutableListOf<Int>()
+  for (i in 0..s.lastIndex - (concatenatedLen - 1)) {
+
+    val substring = if (i + concatenatedLen - 1 == s.length) {
+      s.takeLast(concatenatedLen)
+    } else {
+      (s.substring(i, i + concatenatedLen))
+    }
+    val chunked = substring.chunked(wordLen)
+    val coded = chunked.map { it.hashCode() }
+    val counted = coded.countOccurances()
+
+    val sameElements = counted.all {  entry ->
+      entry.value == wordKeys[entry.key]
+    }
+    if (sameElements) indices += i
+  }
+  return indices
+}
+
+fun String.toKey(): Int {
+  return this.map { (it - 'a') }
+      .reduce { acc, c -> (acc shl 1) + c }
+}
+
+fun Collection<Int>.countOccurances(): Map<Int, Int> {
+  return this.fold(mutableMapOf()) { acc, i ->
+    val value = (acc[i] ?: 0) + 1
+    acc[i] = value
+    acc
+  }
+}
+
+
+
+
+
+
 
 /**
  * Time limit exceeded on giant input with a lot of permutations
  */
-fun findSubstring(s: String, words: Array<String>): List<Int> {
+fun findSubstringSlow(s: String, words: Array<String>): List<Int> {
   return perms(words.toList())
       .toSet()
       .flatMap { s.getIndexesOf(it) }
--- src/test/leetcode/day3/_30_SubstringWithConcatenationOfAllWordsKtTest.kt
+++ src/test/leetcode/day3/_30_SubstringWithConcatenationOfAllWordsKtTest.kt
@@ -1,7 +1,9 @@
 package leetcode.day3
 
 import org.junit.jupiter.api.Test
+import kotlin.system.measureNanoTime
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 internal class _30_SubstringWithConcatenationOfAllWordsKtTest {
 
@@ -10,5 +12,60 @@ internal class _30_SubstringWithConcatenationOfAllWordsKtTest {
     val words = arrayOf("foo", "bar")
     val expected = setOf(0, 3, 6)
     assertEquals(expected, findSubstring(str, words).toSet())
+    assertEquals(expected, findSubstringSlow(str, words).toSet())
+  }
+
+  @Test fun two() {
+    val str = "foofoobarbar"
+    val words = arrayOf("foo", "foo", "bar")
+    val expected = setOf(0)
+    assertEquals(expected, findSubstring(str, words).toSet())
+    assertEquals(expected, findSubstringSlow(str, words).toSet())
+  }
+
+  @Test fun three() {
+    val str = "lingmindraboofooowingdingbarrwingmonkeypoundcake"
+    val words = arrayOf("fooo","barr","wing","ding","wing")
+    val expected = setOf(13)
+    assertEquals(expected, findSubstring(str, words).toSet())
+  }
+
+  @Test fun four() {
+    val str = ""
+    val words = arrayOf("fooo","barr","wing","ding","wing")
+    val expected = setOf<Int>()
+    assertEquals(expected, findSubstring(str, words).toSet())
+  }
+
+  @Test fun five() {
+    val str = "lingmindraboofooowingdingbarrwingmonkeypoundcake"
+    val words = arrayOf<String>()
+    val expected = setOf<Int>()
+    assertEquals(expected, findSubstring(str, words).toSet())
+  }
+
+  @Test fun six() {
+    val str = "bacccaa"
+    val words = arrayOf("cc","ba")
+    val expected = setOf(0)
+    assertEquals(expected, findSubstring(str, words).toSet())
+  }
+
+  @Test fun speed() {
+    val str = "foofoobarbarfoofoobarbarfoofoobarbarfoofoobarbarfoofoobarbarfoofoobarbarfoofoobarbar"
+    val words = arrayOf("foo", "foo", "bar","foo", "foo", "bar","foo")
+    val expected = setOf<Int>()
+
+    val pattern = "%10s"
+
+    val fast = pattern.format(measureNanoTime {
+      assertEquals(expected, findSubstring(str, words).toSet())
+    })
+    val slow = pattern.format(measureNanoTime {
+      assertEquals(expected, findSubstringSlow(str, words).toSet())
+    })
+
+    println("Fast: $fast\nSlow: $slow")
+    assertTrue { fast < slow }
   }
 }
