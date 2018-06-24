330111ce63a17f350be4b69c3debbc562c58c8ea
Baron-Severin <severinrudie@gmail.com>
2018-06-23 19:21:19 -0700

    Implemented RemoveElement

---
+++ src/test/leetcode/day4/_26_RemoveDuplicatesFromSortedArrayKtTest.kt
@@ -0,0 +1,14 @@
+package leetcode.day4
+
+import org.junit.jupiter.api.Test
+
+internal class _26_RemoveDuplicatesFromSortedArrayKtTest {
+
+  @Test
+  fun removeDuplicates() {
+
+
+    val s = listOf(1,2,3,4,5,6,7,8).toSet()
+
+  }
+}
