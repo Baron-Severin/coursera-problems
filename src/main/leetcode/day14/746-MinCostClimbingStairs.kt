cad1873e19190209a543392e6db4a553ea6e34d2
Baron-Severin <severinrudie@gmail.com>
2018-07-04 15:05:59 -0700

    Optimized MinCostClimbingStairs to O(1) space complexity

--- src/main/leetcode/day14/746-MinCostClimbingStairs.kt
+++ src/main/leetcode/day14/746-MinCostClimbingStairs.kt
@@ -3,16 +3,12 @@ package leetcode.day14
 import kotlin.math.min
 
   fun minCostClimbingStairs(cost: IntArray): Int {
-    if (cost.size < 3) {
-      val first = if (cost.size >= 1) cost[0] else 0
-      val second = if (cost.size >= 2) cost[1] else 0
-      return min(first, second)
+    var old = 0
+    var new = 0
+    for (i in cost.indices) {
+      val temp = min(old + cost[i], new + cost[i])
+      old = new
+      new = temp
     }
-    val dp = IntArray(cost.size)
-    dp[0] = cost[0]
-    dp[1] = cost[1]
-    for (i in 2..cost.lastIndex) {
-      dp[i] = min(dp[i - 1] + cost[i], dp[i - 2] + cost[i])
-    }
-    return min(dp.last(), dp[dp.lastIndex - 1])
+    return min(old, new)
   }
