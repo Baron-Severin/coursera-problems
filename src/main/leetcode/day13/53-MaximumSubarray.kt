3f30f7f3b5c513e7dfb61ed0fd06d00a04b5a787
Baron-Severin <severinrudie@gmail.com>
2018-07-03 21:34:30 -0700

    Improved MaximumSubarray implementation

--- src/main/leetcode/day13/53-MaximumSubarray.kt
+++ src/main/leetcode/day13/53-MaximumSubarray.kt
@@ -1,26 +1,39 @@
 package leetcode.day13
 
-  fun maxSubArray(nums: IntArray): Int {
-    var maxSize = Int.MIN_VALUE
-    var currSize = 0
-    var rightIndex = -1
-    for (i in nums.indices) {
-      currSize += nums[i]
-      if (currSize > maxSize) {
-        maxSize = currSize
-        rightIndex = i
-      }
-    }
+import kotlin.math.max
 
-    maxSize = 0
-    currSize = 0
-    var leftIndex = -1
-    for (i in rightIndex downTo 0) {
-      currSize += nums[i]
-      if (currSize > maxSize) {
-        maxSize = currSize
-        leftIndex = i
-      }
-    }
-    return nums.toList().subList(leftIndex, rightIndex + 1).sum()
+
+fun maxSubArray(nums: IntArray): Int {
+  val dp = IntArray(nums.size)
+  for (i in nums.indices) {
+    val last = if (i != 0) dp[i - 1] else 0
+    dp[i] = max(last + nums[i], nums[i])
   }
+  return dp.max() ?: 0
+}
+
+ // o(n) solution
+//  fun maxSubArray(nums: IntArray): Int {
+//    var maxSize = Int.MIN_VALUE
+//    var currSize = nums[0]
+//    var rightIndex = -1
+//    for (i in nums.indices) {
+//      currSize += nums[i]
+//      if (currSize > maxSize) {
+//        maxSize = currSize
+//        rightIndex = i
+//      }
+//    }
+//
+//    maxSize = Int.MIN_VALUE
+//    currSize = nums[rightIndex]
+//    var leftIndex = -1
+//    for (i in rightIndex downTo 0) {
+//      currSize += nums[i]
+//      if (currSize > maxSize) {
+//        maxSize = currSize
+//        leftIndex = i
+//      }
+//    }
+//    return nums.toList().subList(leftIndex, rightIndex + 1).sum()
+//  }
--- src/test/leetcode/day13/_53_MaximumSubarrayKtTest.kt
+++ src/test/leetcode/day13/_53_MaximumSubarrayKtTest.kt
@@ -5,13 +5,16 @@ import kotlin.test.assertEquals
 
 internal class _53_MaximumSubarrayKtTest {
 
-  @Test
-  fun one() {
+  @Test fun one() {
     val input = intArrayOf(-2,1,-3,4,-1,2,1,-5,4)
     val expected = 6
     assertEquals(expected, maxSubArray(input))
   }
   
-  
+  @Test fun two() {
+    val input = intArrayOf(-1)
+    val expected = -1
+    assertEquals(expected, maxSubArray(input))
+  }
   
 }
